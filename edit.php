ee8b7b9049a03a0a8dab01b889c59d91b53f21d4
panithan-kongthong <73686785+panithan-kongthong@users.noreply.github.com>
2020-11-27 14:46:57 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -3,7 +3,7 @@
 
     $id = $_GET['ID'];
 
-    $sql = 'SELECT * FROM guestbook WHERE ID = ' . $id . '';
+    $sql = 'SELECT * FROM labtest WHERE ID = ' . $id . '';
     $query = mysqli_query($conn, $sql);
     if (!$query) {
         header('Location: index.php');
